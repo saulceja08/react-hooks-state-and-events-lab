5e823b7f013b08c0e534efe4c41ba3ffb5edfec0
Saul Ceja <saulceja08@gmail.com>
2023-06-10 20:40:56 -0700

    Complete Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [message, setMessage] = useState(false);
+  const cartMessage = message ? 'Remove From Cart' : 'Add to Cart';
+  const classCart = message ? 'in-cart' : "";
+
+  const cartStatus = () => {
+    setMessage(!message);
+  };
+
   return (
-    <li className="">
+    <li className={classCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={cartStatus}>
+        {cartMessage}
+      </button>
     </li>
   );
 }
